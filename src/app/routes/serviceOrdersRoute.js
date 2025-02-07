741f106cc7832de71f7447f381a82840a1144953
Leonardo Kotcheski Filipiaki <duckgames875@gmail.com>
2025-02-06 22:20:16 -0300

    Criação das Rotas, Model e Controller para o controle de ordens de serviço (OS ou Service Orders)

---
+++ src/app/routes/serviceOrdersRoute.js
@@ -0,0 +1,63 @@
+const router = require('express').Router();
+const OSController = require('../controllers/serviceOrdersController');
+
+router.post('/create', async (req, res) => {
+    let result = await OSController.createOS(req.body);
+    if(Array.isArray(result)){
+        res.status(403).send(result);
+    }else if(result == 201){
+        res.status(201).send('OS creation went successfull');
+    }else{
+        res.status(500).send('System error, try again or contact your supervisor');
+    }
+})
+
+// router.get('/find', async(req, res) => {
+//     let result = await OSController.findOS(req.body);
+    
+//     if(!result.data){
+//         res.status(500).send('System error, try again or contact your supervisor');
+//     }else{
+//         res.status(result.httpRes).send(result.data);
+//     }
+// })
+
+router.get('/findAll', async(req, res) => {
+    let result = await OSController.findAllServiceOrders();
+    if(!result.data){
+        res.status(500).send('System error, try again or contact your supervisor');
+    }else{
+        res.status(result.httpRes).send(result.data);
+    }
+})
+
+// router.patch('/update/:id', async (req, res) => {
+//     const id = req.params.id;
+    
+//     let result = await OSController.updateCustomer(id, req.body);
+    
+//     if(Array.isArray(result)){
+//         res.status(result[0].httpRes).send(result[0].data);
+//     }
+//     if(result.httpRes){
+//         res.status(result.httpRes).send(result.data);
+//     }else{
+//         res.status(500).send(result.data);
+//     }
+
+// })
+
+// router.delete('/delete/:id', async (req, res) => {
+//     const id = req.params.id;
+
+//     let result = await OSController.deleteCustomer(id);
+//     if(Array.isArray(result)){
+//         res.status(result[0].httpRes).send(result[0].data);
+//     }
+//     if(result.httpRes){
+//         res.status(result.httpRes).send(result.data);
+//     }else{
+//         res.status(500).send('Server error');
+//     }
+// })
+module.exports = router;
